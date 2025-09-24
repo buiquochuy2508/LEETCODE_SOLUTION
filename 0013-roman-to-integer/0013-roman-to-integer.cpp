d527092195c7357ae1ac5c97922599940b1e3595
Bui Quoc Huy <wuochuy02@gmail.com>
2025-09-24 19:05:38 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        int res = 0;
+        map <char, int> mp;
+        mp['I'] = 1; mp['V'] = 5; mp['X'] = 10;
+        mp['L'] = 50; mp['C'] = 100;
+        mp['D'] = 500; mp['M'] = 1000;
+        //res += mp[s[0]];
+        bool ok = false;
+        for (int i = 0; i < s.size() - 1; i++){
+            if (mp[s[i]] < mp[s[i + 1]]){
+                res += mp[s[i + 1]] - mp[s[i]];
+                ++i;
+                ok = true;
+            }else{
+                res += mp[s[i]];
+                ok = false;
+            }
+        }
+        //cout << ok;
+        //if (mp[s[s.size() - 1]] >= mp[s[s.size() - 2]]) res += mp[s[s.size() - 1]];
+        if (!ok) res += mp[s[s.size() - 1]];
+        return res;
+    }
+};
